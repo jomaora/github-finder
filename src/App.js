a42c2832f8a54d1ae8636d6c434ad1a888b4158d
Joan Ortega <jomaora@gmail.com>
2023-10-18 16:06:44 +0200

    section 8 code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/Footer";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
+
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className="text-xl">Hello World</h1>
-      <button className="btn">Click</button>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar/>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      Some explanations.
+    </div>
+  )
+}
+
+export default About;
