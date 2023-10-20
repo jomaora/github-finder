d500f9c0c35e3716b40c82bad28c5c3b54bec5a4
Joan Ortega <jomaora@gmail.com>
2023-10-20 15:57:40 +0200

    Using reducers - clear states - sessin 55

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,4 +1,5 @@
-import {createContext, useState} from "react";
+import {createContext, useReducer} from "react";
+import { githubReducer } from "./GithubReducer";
 
 const GithubContext = createContext();
 
@@ -6,10 +7,15 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({children}) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const initialState = {
+    users: [],
+    loading: false
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
+    setLoading();
     const response = await fetch(`${GITHUB_URL}/users`, 
       {
         headers: {
@@ -18,15 +24,16 @@ export const GithubProvider = ({children}) => {
       }
     );
     const data = await response.json();
-    setUsers(data);
-    setLoading(false);
-    return data;
+    dispatch({type: 'GET_USERS', payload: data});
   }
 
+  const setLoading = () => dispatch({type: 'SET_LOADING'});
+
   return <GithubContext.Provider value={{
-    users, 
-    loading,
-    fetchUsers
+    users: state.users, 
+    loading: state.loading,
+    fetchUsers,
+    setLoading
   }}>
     {children}
   </GithubContext.Provider>
