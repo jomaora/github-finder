728fa2a925729e386e8b38c6de2efb15b5ab2c23
Joan Ortega <jomaora@gmail.com>
2023-11-02 11:45:25 +0100

    refactor using axios

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,17 +1,19 @@
+import axios from "axios";
+
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
+const githubInstance = axios.create({
+  baseURL: GITHUB_URL,
+  headers: {
+    Authorization: `Bearer ${GITHUB_TOKEN}`
+  }
+})
+
 export const searchUsers = async (query) => {
   const params = new URLSearchParams({q: query})
-  const response = await fetch(`${GITHUB_URL}/search/users?${params}`, 
-    {
-      headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}`
-      }
-    }
-  );
-  const {items} = await response.json();
-  return items;
+  const response = await githubInstance.get(`/search/users?${params}`)
+  return response.data.items;
 }
 
 export const loadUser = async login => {
